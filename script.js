001500fa83e0372ab76bec4e53d4c0ee9ce5546c
Juan <juanmartinsr07@gmail.com>
2026-03-11 15:39:56 -0300

    portfolio atualizado

--- script.js
+++ script.js
@@ -1,6 +1,33 @@
 const toggle = document.querySelector('.menu-toggle');
 const navLinks = document.querySelector('.nav-links');
+const links = document.querySelectorAll('.nav-links a');
 
 toggle.addEventListener('click', () => {
     navLinks.classList.toggle('active');
+});
+
+// fecha o menu quando clicar em um link
+links.forEach(link => {
+    link.addEventListener('click', () => {
+        navLinks.classList.remove('active');
+    });
+});
+
+
+const projectCards = document.querySelectorAll('.project-card');
+const observer = new IntersectionObserver((entries) => {
+
+    entries.forEach(entry => {
+
+        if (entry.isIntersecting) {
+            entry.target.classList.add('visible');
+        }
+    });
+
+}, {
+    threshold: 0.3
+});
+
+projectCards.forEach(card => {
+    observer.observe(card);
 });
